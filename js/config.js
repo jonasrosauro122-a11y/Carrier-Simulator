a41f2866836deb4d526308966a32a90858acb63f
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2026-05-19 17:27:53 -0400

    Update Supabase URL and anon key in config.js

--- js/config.js
+++ js/config.js
@@ -8,10 +8,9 @@
   5. Commit this file to GitHub only if this is a training/demo project.
 */
 window.LAVA_SUPABASE = {
-  url: "",       // Example: "https://abcxyz.supabase.co"
-  anonKey: "",   // Example: "eyJhbGciOi..."
+  url: "https://veebxxhumvnkgmcszinh.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZWJ4eGh1bXZua2dtY3N6aW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NTA3OTIsImV4cCI6MjA5MjUyNjc5Mn0.sEnWsSrvNRBmarGnvlNPJdlmOii2t88UdfUmy7cDhl0",
   bucket: "carrier-documents"
 };
 
-// Training access code for Trainer/TL view.
 window.LAVA_TRAINER_CODE = "LAVA2026";
